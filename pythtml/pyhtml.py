b4fe45462706f1c47ed6fbd56fe0f770b6967825
Ajax1234 <pentester18@gmail.com>
2017-11-24 17:02:52 -0500

    Update pyhtml.py

--- pythtml/pyhtml.py
+++ pythtml/pyhtml.py
@@ -1,5 +1,153 @@
 import re
-
+css3_names_to_hex = {
+    'aliceblue': '#f0f8ff',
+    'antiquewhite': '#faebd7',
+    'aqua': '#00ffff',
+    'aquamarine': '#7fffd4',
+    'azure': '#f0ffff',
+    'beige': '#f5f5dc',
+    'bisque': '#ffe4c4',
+    'black': '#000000',
+    'blanchedalmond': '#ffebcd',
+    'blue': '#0000ff',
+    'blueviolet': '#8a2be2',
+    'brown': '#a52a2a',
+    'burlywood': '#deb887',
+    'cadetblue': '#5f9ea0',
+    'chartreuse': '#7fff00',
+    'chocolate': '#d2691e',
+    'coral': '#ff7f50',
+    'cornflowerblue': '#6495ed',
+    'cornsilk': '#fff8dc',
+    'crimson': '#dc143c',
+    'cyan': '#00ffff',
+    'darkblue': '#00008b',
+    'darkcyan': '#008b8b',
+    'darkgoldenrod': '#b8860b',
+    'darkgray': '#a9a9a9',
+    'darkgrey': '#a9a9a9',
+    'darkgreen': '#006400',
+    'darkkhaki': '#bdb76b',
+    'darkmagenta': '#8b008b',
+    'darkolivegreen': '#556b2f',
+    'darkorange': '#ff8c00',
+    'darkorchid': '#9932cc',
+    'darkred': '#8b0000',
+    'darksalmon': '#e9967a',
+    'darkseagreen': '#8fbc8f',
+    'darkslateblue': '#483d8b',
+    'darkslategray': '#2f4f4f',
+    'darkslategrey': '#2f4f4f',
+    'darkturquoise': '#00ced1',
+    'darkviolet': '#9400d3',
+    'deeppink': '#ff1493',
+    'deepskyblue': '#00bfff',
+    'dimgray': '#696969',
+    'dimgrey': '#696969',
+    'dodgerblue': '#1e90ff',
+    'firebrick': '#b22222',
+    'floralwhite': '#fffaf0',
+    'forestgreen': '#228b22',
+    'fuchsia': '#ff00ff',
+    'gainsboro': '#dcdcdc',
+    'ghostwhite': '#f8f8ff',
+    'gold': '#ffd700',
+    'goldenrod': '#daa520',
+    'gray': '#808080',
+    'grey': '#808080',
+    'green': '#008000',
+    'greenyellow': '#adff2f',
+    'honeydew': '#f0fff0',
+    'hotpink': '#ff69b4',
+    'indianred': '#cd5c5c',
+    'indigo': '#4b0082',
+    'ivory': '#fffff0',
+    'khaki': '#f0e68c',
+    'lavender': '#e6e6fa',
+    'lavenderblush': '#fff0f5',
+    'lawngreen': '#7cfc00',
+    'lemonchiffon': '#fffacd',
+    'lightblue': '#add8e6',
+    'lightcoral': '#f08080',
+    'lightcyan': '#e0ffff',
+    'lightgoldenrodyellow': '#fafad2',
+    'lightgray': '#d3d3d3',
+    'lightgrey': '#d3d3d3',
+    'lightgreen': '#90ee90',
+    'lightpink': '#ffb6c1',
+    'lightsalmon': '#ffa07a',
+    'lightseagreen': '#20b2aa',
+    'lightskyblue': '#87cefa',
+    'lightslategray': '#778899',
+    'lightslategrey': '#778899',
+    'lightsteelblue': '#b0c4de',
+    'lightyellow': '#ffffe0',
+    'lime': '#00ff00',
+    'limegreen': '#32cd32',
+    'linen': '#faf0e6',
+    'magenta': '#ff00ff',
+    'maroon': '#800000',
+    'mediumaquamarine': '#66cdaa',
+    'mediumblue': '#0000cd',
+    'mediumorchid': '#ba55d3',
+    'mediumpurple': '#9370d8',
+    'mediumseagreen': '#3cb371',
+    'mediumslateblue': '#7b68ee',
+    'mediumspringgreen': '#00fa9a',
+    'mediumturquoise': '#48d1cc',
+    'mediumvioletred': '#c71585',
+    'midnightblue': '#191970',
+    'mintcream': '#f5fffa',
+    'mistyrose': '#ffe4e1',
+    'moccasin': '#ffe4b5',
+    'navajowhite': '#ffdead',
+    'navy': '#000080',
+    'oldlace': '#fdf5e6',
+    'olive': '#808000',
+    'olivedrab': '#6b8e23',
+    'orange': '#ffa500',
+    'orangered': '#ff4500',
+    'orchid': '#da70d6',
+    'palegoldenrod': '#eee8aa',
+    'palegreen': '#98fb98',
+    'paleturquoise': '#afeeee',
+    'palevioletred': '#d87093',
+    'papayawhip': '#ffefd5',
+    'peachpuff': '#ffdab9',
+    'peru': '#cd853f',
+    'pink': '#ffc0cb',
+    'plum': '#dda0dd',
+    'powderblue': '#b0e0e6',
+    'purple': '#800080',
+    'red': '#ff0000',
+    'rosybrown': '#bc8f8f',
+    'royalblue': '#4169e1',
+    'saddlebrown': '#8b4513',
+    'salmon': '#fa8072',
+    'sandybrown': '#f4a460',
+    'seagreen': '#2e8b57',
+    'seashell': '#fff5ee',
+    'sienna': '#a0522d',
+    'silver': '#c0c0c0',
+    'skyblue': '#87ceeb',
+    'slateblue': '#6a5acd',
+    'slategray': '#708090',
+    'slategrey': '#708090',
+    'snow': '#fffafa',
+    'springgreen': '#00ff7f',
+    'steelblue': '#4682b4',
+    'tan': '#d2b48c',
+    'teal': '#008080',
+    'thistle': '#d8bfd8',
+    'tomato': '#ff6347',
+    'turquoise': '#40e0d0',
+    'violet': '#ee82ee',
+    'wheat': '#f5deb3',
+    'white': '#ffffff',
+    'whitesmoke': '#f5f5f5',
+    'yellow': '#ffff00',
+    'yellowgreen': '#9acd32',
+}
 class InvalidFileName(Exception):
     def __str__(self):
         return "Filname must end with '.html'"
@@ -31,6 +179,8 @@ class Button(CSS):
 
     def button(self, text, classname, onclick="", width="auto", height="auto", border_radius = "auto", background_color="auto", color="auto", padding="auto", position = "auto", text_align='auto',text_decoration="auto", font_size="auto", margin="auto", margin_top="auto", margin_left="auto", margin_right="auto", margin_bottom="auto"):
         self.buttonname = classname
+        background_color = background_color if background_color == "auto" else css3_names_to_hex[background_color.lower()] if not background_color.startswith('#') else background_color
+        color = color if color == "auto" else css3_names_to_hex[color.lower()] if not color.startswith('#') else color
         self.header = "<button class='{}' onclick = '{}' style='border-radius:{};background-color:{};color:{};padding:{};position:{};text-align:{};text-decoration:{};font-size:{};margin:{};margin-top:{};margin-left:{};margin-right:{};margin-bottom:{};width:{};height:{};'>{}</button>\n\n".format(classname, onclick, border_radius, background_color, color, padding, position, text_align,text_decoration, font_size, margin, margin_top, margin_left, margin_right, margin_bottom, width, height,text)
         #classname, background_color, color, padding, position, text_align,text_decoration, font_size, margin, margin_top, margin_left, margin_right, margin_bottom
     def hover(self, **kwargs):
@@ -113,7 +263,8 @@ class Containers:
 
         if re.findall('^h\d+$|^p\d+$|strong', name):
             def wrapper(text, color="black", font_size="auto", width="auto", height="auto", text_align="auto", font_family="auto", position="auto", margin="auto", margin_left="auto", margin_right="auto", margin_top="auto", margin_bottom="auto", font_weight="auto"):
-
+                #background_color = background_color if background_color == "auto" else css3_names_to_hex[background_color] if not background_color.startswith('#') else background_color
+                color = color if color == "auto" else css3_names_to_hex[color.lower()] if not color.startswith('#') else color
                 line = "<{} style='width:{};height:{};font-size:{};text-align:{};position:{};font-family:{};color:{};margin:{};margin-left:{};margin-right:{};margin-bottom:{};margin-top:{};font-weight:{};'>{}</{}>".format(name, width, height, font_size, text_align, position, font_family, color, margin, margin_left, margin_right, margin_bottom, margin_top, font_weight, text, name)
                 self.filecontent += '\n\n'+line
             return wrapper
@@ -121,6 +272,8 @@ class Containers:
 
         if name == "ol" or name == "ul":
             def wrapper(items = [], width="auto", color="auto", height="auto", font_weight="auto", text_align="auto", font_size="auto",font_family="auto", position="auto", margin="auto", margin_left="auto", margin_right="auto", margin_top="auto", margin_bottom="auto", **elements):
+                #background_color = background_color if background_color == "auto" else css3_names_to_hex[background_color] if not background_color.startswith('#') else background_color
+                color = color if color == "auto" else css3_names_to_hex[color.lower()] if not color.startswith('#') else color
                 header = "<{} style='width:{};height:{};font-size:{};text-align:{};position:{};font-family:{};color:{};margin:{};margin-left:{};margin-right:{};margin-bottom:{};margin-top:{};font-weight:{};'>".format(name, width, height, font_size, text_align, position, font_family, color, margin, margin_left, margin_right, margin_bottom, margin_top, font_weight)
                 bottom = "</{}>".format(name)
                 full_listing = '\n\n'.join('<li style=color:{};front-size:{};font-family:{};font-weight:{}>{}</li>'.format(c, f, ff, fw, i) for c, f, ff, fw, i in zip(elements.get("colors", ["auto"]*len(items)), elements.get("font", ["auto"]*len(items)), elements.get("font-family", ['auto']*len(items)),  elements.get("font-weight", ['auto']*len(items)), items))
@@ -168,7 +321,8 @@ class Form(Containers):
 
         if re.findall('^h\d+$|^p\d+$|strong', name):
             def wrapper(text, color="black", font_size="auto", width="auto", height="auto", text_align="auto", font_family="auto", position="auto", margin="auto", margin_left="auto", margin_right="auto", margin_top="auto", margin_bottom="auto", font_weight="auto"):
-
+                #background_color = background_color if background_color == "auto" else css3_names_to_hex[background_color] if not background_color.startswith('#') else background_color
+                color = color if color == "auto" else css3_names_to_hex[color.lower()] if not color.startswith('#') else color
                 line = "<{} style='width:{};height:{};font-size:{};text-align:{};position:{};font-family:{};color:{};margin:{};margin-left:{};margin-right:{};margin-bottom:{};margin-top:{};font-weight:{};'>{}</{}>".format(name, width, height, font_size, text_align, position, font_family, color, margin, margin_left, margin_right, margin_bottom, margin_top, font_weight, text, name)
                 self.form_header += '\n\n'+line
             return wrapper
@@ -176,6 +330,8 @@ class Form(Containers):
 
         if name == "ol" or name == "ul":
             def wrapper(items = [], width="auto", color="auto", height="auto", font_weight="auto", text_align="auto", font_size="auto",font_family="auto", position="auto", margin="auto", margin_left="auto", margin_right="auto", margin_top="auto", margin_bottom="auto", **elements):
+                #background_color = background_color if background_color == "auto" else css3_names_to_hex[background_color] if not background_color.startswith('#') else background_color
+                color = color if color == "auto" else css3_names_to_hex[color.lower()] if not color.startswith('#') else color
                 header = "<{} style='width:{};height:{};font-size:{};text-align:{};position:{};font-family:{};color:{};margin:{};margin-left:{};margin-right:{};margin-bottom:{};margin-top:{};font-weight:{};'>".format(name, width, height, font_size, text_align, position, font_family, color, margin, margin_left, margin_right, margin_bottom, margin_top, font_weight)
                 bottom = "</{}>".format(name)
                 full_listing = '\n\n'.join('<li style=color:{};front-size:{};font-family:{};font-weight:{}>{}</li>'.format(c, f, ff, fw, i) for c, f, ff, fw, i in zip(elements.get("colors", ["auto"]*len(items)), elements.get("font", ["auto"]*len(items)), elements.get("font-family", ['auto']*len(items)),  elements.get("font-weight", ['auto']*len(items)), items))
@@ -200,7 +356,8 @@ class div:
 
 
     def a(self, link="#", text="", color="black", font_size="auto", width="auto", height="auto", text_align="auto", font_family="auto", position="auto", margin="auto", margin_left="auto", margin_right="auto", margin_top="auto", margin_bottom="auto", font_weight="auto", text_decoration="auto"):
-
+        #background_color = background_color if background_color == "auto" else css3_names_to_hex[background_color.lower()] if not background_color.startswith('#') else background_color
+        color = color if color == "auto" else css3_names_to_hex[color.lower()] if not color.startswith('#') else color
         line = "<a href='{}' style='width:{};height:{};font-size:{};text-align:{};position:{};font-family:{};color:{};margin:{};margin-left:{};margin-right:{};margin-bottom:{};margin-top:{};font-weight:{};text-decoration:{};'>{}</a>\n".format(link, width, height, font_size, text_align, position, font_family, color, margin, margin_left, margin_right, margin_bottom, margin_top, font_weight, text_decoration, text)
         self.filecontent += '\n\n'+line
 
@@ -219,6 +376,8 @@ class div:
 
         if name == "ol" or name == "ul":
             def wrapper(items = [], width="auto", color="auto", height="auto", font_weight="auto", text_align="auto", font_size="auto",font_family="auto", position="auto", margin="auto", margin_left="auto", margin_right="auto", margin_top="auto", margin_bottom="auto", **elements):
+                #background_color = background_color if background_color == "auto" else css3_names_to_hex[background_color] if not background_color.startswith('#') else background_color
+                color = color if color == "auto" else css3_names_to_hex[color.lower()] if not color.startswith('#') else color
                 header = "<{} style='width:{};height:{};font-size:{};text-align:{};position:{};font-family:{};color:{};margin:{};margin-left:{};margin-right:{};margin-bottom:{};margin-top:{};font-weight:{};'>".format(name, width, height, font_size, text_align, position, font_family, color, margin, margin_left, margin_right, margin_bottom, margin_top, font_weight)
                 bottom = "</{}>".format(name)
                 full_listing = '\n\n'.join('<li style=color:{};front-size:{};font-family:{};font-weight:{}>{}</li>'.format(c, f, ff, fw, i) for c, f, ff, fw, i in zip(elements.get("colors", ["auto"]*len(items)), elements.get("font", ["auto"]*len(items)), elements.get("font-family", ['auto']*len(items)),  elements.get("font-weight", ['auto']*len(items)), items))
@@ -238,6 +397,8 @@ class Body(Containers):
     #IDEA: allow user to pass style object
     def __init__(self, background_color="auto", font_size="auto", width="auto", height="auto", text_align="auto", font_family="auto", position="auto", margin="auto", margin_left="auto", margin_right="auto", margin_top="auto", margin_bottom="auto", font_weight="auto"):
         Containers.__init__(self)
+        background_color = background_color if background_color == "auto" else css3_names_to_hex[background_color.lower()] if not background_color.startswith('#') else background_color
+        #color = color if color == "auto" else css3_names_to_hex[color] if not color.startswith('#') else color
         self.closing_brackets = []
         self.body_header = "<body style = 'background-color:{};width:{};height:{};font-size:{};text-align:{};position:{};font-family:{};margin:{};margin-left:{};margin-right:{};margin-bottom:{};margin-top:{};font-weight:{};'>".format(background_color, width, height, font_size, text_align, position, font_family, margin, margin_left, margin_right, margin_bottom, margin_top, font_weight)
         self.closing_brackets.append("</body>")
@@ -315,4 +476,3 @@ class Body(Containers):
         self.filecontent = "<html>\n"+self.style+'\n' + self.filecontent
         self.filecontent += self.script
         self.filecontent += "\n</html>"
-
